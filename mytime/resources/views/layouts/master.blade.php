fe03d2f972e865cba687b3de8df17eedb6700469
”YuYuYuhei” <yuuhei.kubo7@gmail.com>
2019-05-20 01:27:25 +0900

    ログイン周り

--- mytime/resources/views/layouts/master.blade.php
+++ mytime/resources/views/layouts/master.blade.php
@@ -15,8 +15,52 @@
 
     </head>
     <body>
-        <div class="container">
-            @yield('content')
-        </div>
+        <header>
+            <!-- ナビバー -->
+            <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+                <a href="/" class="navbar-brand">Logo</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu" >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div id="menu" class="collapse navbar-collapse">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item"><a href="" class="nav-link">ホーム</a></li>
+                        <li class="nav-item"><a href="" class="nav-link">新規作成</a></li>
+                    </ul>
+                    <!-- ナビバー右側 -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                        <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
+                        @if (Route::has('register'))
+                        <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                        @endif
+                        @else
+                        <!-- ログアウト機能が不全になった時のログアウト記述 -->
+                        <!--<li><a class="nav-link" href="{{ route('logout') }}">{{ __('Logout') }}</a></li>-->
+                        <div aria-labelledby="navbarDropdown">
+                            <a class="nav-link" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                        @endguest
+                    </ul>
+                </div>
+            </nav>
+            <!-- ナビバーここまで -->
+        </header>
+        <main>
+            <div class="container">
+                @yield('content')
+            </div>
+        </main>
+        <footer>
+        </footer>
     </body>
 </html>
--- mytime/routes/web.php
+++ mytime/routes/web.php
@@ -12,7 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
-Route::get('record', 'RecordController@index');
+Route::get('record', 'RecordController@index')->middleware('auth');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
